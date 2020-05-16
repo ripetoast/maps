48ef82ccfaf47a97a4fee14e95787bfdd4069514
Michael <michael_lau@live.co.uk>
2020-05-17 00:13:29 +0100

    ADD: Interface in CustomMap class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-new CustomMap('map');
+const user = new User();
+const customMap = new CustomMap('map');
+const company = new Company();
+customMap.addMarker(user);
+customMap.addMarker(company);
